2f4b7780b4484b1b3c971412c9ed1a3be5da8c99
oguzademoglu <oguzademoglu1@gmail.com>
2025-08-14 15:14:32 +0300

    feat(player): core states (Grounded/Idle/Move) + AnimatorController + idle anim; scene hookup

--- Assets/LV01/Scripts/EntityState.cs
+++ Assets/LV01/Scripts/EntityState.cs
@@ -5,14 +5,25 @@ public abstract class EntityState
     protected Player player;
     protected StateMachine stateMachine;
     protected string stateName;
+    protected Rigidbody2D rb;
+    protected Animator anim;
     public EntityState(Player player, StateMachine stateMachine, string stateName)
     {
         this.player = player;
         this.stateMachine = stateMachine;
         this.stateName = stateName;
+        rb = player.Rb;
+        anim = player.Anim;
     }
 
-    public virtual void Enter() { }
+    public virtual void Enter()
+    {
+        anim.SetBool(stateName, true);
+    }
     public virtual void Update() { }
-    public virtual void Exit() { }
+    public virtual void Exit()
+    {
+        anim.SetBool(stateName, false);
+    }
+
 }
--- Assets/LV01/Scripts/Player.cs
+++ Assets/LV01/Scripts/Player.cs
@@ -1,23 +1,69 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public StateMachine StateMachine { get; private set; }
+    public PlayerInputs PlayerInputs { get; private set; }
+    public Player_IdleState IdleState { get; private set; }
+    public Player_MoveState MoveState { get; private set; }
+    [Header("Player Components")]
     public Rigidbody2D Rb { get; private set; }
     public Animator Anim { get; private set; }
+    [Header("Movement Details")]
+    public Vector2 MoveInput { get; private set; }
+    public float moveSpeed;
+    private bool facingRight = true;
+    public int facingDirection = 1;
 
     void Awake()
     {
         Rb = GetComponent<Rigidbody2D>();
         Anim = GetComponentInChildren<Animator>();
+        StateMachine = new StateMachine();
+        PlayerInputs = new PlayerInputs();
+        IdleState = new Player_IdleState(this, StateMachine, "idle");
+        MoveState = new Player_MoveState(this, StateMachine, "move");
+    }
+    void OnEnable()
+    {
+        PlayerInputs.Enable();
+
+        PlayerInputs.Player.Movement.performed += ctx => MoveInput = ctx.ReadValue<Vector2>();
+        PlayerInputs.Player.Movement.canceled += ctx => MoveInput = Vector2.zero;
+    }
+    void OnDisable()
+    {
+        PlayerInputs.Disable();
     }
     void Start()
     {
-
+        StateMachine.InitializeState(IdleState);
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        StateMachine.UpdateActiveState();
+    }
 
+
+    public void SetVelocity(float xVelocity, float yVelocity)
+    {
+        Rb.linearVelocity = new Vector2(xVelocity, yVelocity);
+        HandleFlip(xVelocity);
+    }
+
+    void HandleFlip(float xVelocity)
+    {
+        if (xVelocity < 0 && facingRight) Flip();
+        else if (xVelocity > 0 && facingRight == false) Flip();
+    }
+
+    public void Flip()
+    {
+        transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
+        facingDirection *= -1;
     }
 }
